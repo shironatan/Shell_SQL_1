d1c448bcbb4d9d614d67e725d1a85d2a38478d46
root <467tn.mi@gmail.com>
2018-12-04 23:42:01 +0900

    naosita

--- Show_sql/Login.sh
+++ Show_sql/Login.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#ユーザー変更
+#ユーザー新規作成・変更
 Change(){
         local file=".my.cnf"
 	local username
 	local password
 	local hostname
 
-	echo "ユーザーを変更"
+	echo "ユーザーを新規作成・変更"
 	read -p "ユーザー名 : " username </dev/tty
 	read -s -p "パスワード : " password </dev/tty
 	echo
@@ -18,9 +18,8 @@ Change(){
 		echo "password = $password";
 		echo "host = $hostname";
 	} > $file
-	echo "変更完了"
+	echo "新規作成・変更完了"
 }
-
 #ログインユーザー選ぶ
 Account(){
 	local file=".my.cnf"
